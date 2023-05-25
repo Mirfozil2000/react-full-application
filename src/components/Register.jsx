cf8b9e655e38b160d4d78186611f18390e4b1d1c
Mirfozil <mirfoziln@gmail.com>
2023-05-25 13:45:50 +0500

    api and redux register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import { logo } from "../constants";
 import { Input } from "../ui";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUserStart } from "../slice/auth";
 
 const Register = () => {
 
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const dispatch = useDispatch();
+  const {isLoading} = useSelector(state => state.auth)
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    dispatch(registerUserStart())
+  }
 
   return (
     <div className="text-center mt-5">
@@ -26,8 +36,8 @@ const Register = () => {
           <Input label={'Password'} type={'password'} state={password} setState={setPassword}/>
 
          
-          <button className="w-100 btn btn-lg btn-primary mt-2" type="submit">
-            Register
+          <button className="w-100 btn btn-lg btn-primary mt-2" disabled={isLoading} onClick={loginHandler} type="submit">
+            {isLoading ? 'Loading...' : 'Register'}
           </button>
           <p className="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
         </form>
--- src/slice/auth.js
+++ src/slice/auth.js
@@ -1,22 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    isLoading: false,
-    isLoggedIn: false,
-    user: null,
-}
+  isLoading: false,
+  isLoggedIn: false,
+  user: null,
+};
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        loginUserStart: state => {
-            state.isLoading = true
-        },
-        loginUserSuccess: state => {},
-        loginUserFailure: state => {},
-    } 
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    // login
+    loginUserStart: (state) => {
+      state.isLoading = true;
+    },
+    loginUserSuccess: (state) => {},
+    loginUserFailure: (state) => {},
+    // register
+    registerUserStart: (state) => {
+      state.isLoading = true;
+    },
+    registerUserSuccess: (state) => {},
+    registerUserFailure: (state) => {},
+  },
+});
 
-export const {loginUserStart} = authSlice.actions;
+export const { loginUserStart, registerUserStart } = authSlice.actions;
 export default authSlice.reducer;
