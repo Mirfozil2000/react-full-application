6ad04a6c4e3642e41e12e0d23ed1d7d6acf80718
Mirfozil <mirfoziln@gmail.com>
2023-05-25 14:09:58 +0500

    register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,21 +2,34 @@ import React, { useState } from "react";
 import { logo } from "../constants";
 import { Input } from "../ui";
 import { useDispatch, useSelector } from "react-redux";
-import { registerUserStart } from "../slice/auth";
+import {
+  registerUserFailure,
+  registerUserStart,
+  registerUserSuccess,
+} from "../slice/auth";
+import AuthService from "../service/auth.service";
 
 const Register = () => {
-
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const dispatch = useDispatch();
-  const {isLoading} = useSelector(state => state.auth)
+  const { isLoading } = useSelector((state) => state.auth);
 
-  const loginHandler = (e) => {
+  const loginHandler = async (e) => {
     e.preventDefault();
-    dispatch(registerUserStart())
-  }
+    dispatch(registerUserStart());
+    const user = {username: name, email, password};
+    try {
+      const response = await AuthService.userRegister(user)
+      console.log(response);
+      console.log(user);
+      dispatch(registerUserSuccess());
+    } catch (error) {
+      dispatch(registerUserFailure());
+    }
+  };
 
   return (
     <div className="text-center mt-5">
@@ -31,13 +44,27 @@ const Register = () => {
           />
           <h1 className="h3 mb-3 fw-normal">Please Register</h1>
 
-          <Input label={'Username'} state={name} setState={setName}/>
-          <Input label={'Email address'} type={'email'} state={email} setState={setEmail}/>
-          <Input label={'Password'} type={'password'} state={password} setState={setPassword}/>
+          <Input label={"Username"} state={name} setState={setName} />
+          <Input
+            label={"Email address"}
+            type={"email"}
+            state={email}
+            setState={setEmail}
+          />
+          <Input
+            label={"Password"}
+            type={"password"}
+            state={password}
+            setState={setPassword}
+          />
 
-         
-          <button className="w-100 btn btn-lg btn-primary mt-2" disabled={isLoading} onClick={loginHandler} type="submit">
-            {isLoading ? 'Loading...' : 'Register'}
+          <button
+            className="w-100 btn btn-lg btn-primary mt-2"
+            disabled={isLoading}
+            onClick={loginHandler}
+            type="submit"
+          >
+            {isLoading ? "Loading..." : "Register"}
           </button>
           <p className="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
         </form>
