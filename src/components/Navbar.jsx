64f14ba52a169fb0a511ce1bb1817ba42713e22f
Mirfozil <mirfoziln@gmail.com>
2023-05-26 12:38:02 +0500

    add logout logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { logo } from "../constants";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { removeItem } from "../helpers/persistance-storage";
+import { logoutUser } from "../slice/auth";
 
 const Navbar = () => {
   const { isLoggedIn, user } = useSelector((state) => state.auth);
+
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logoutUser())
+    removeItem('token')
+    navigate('/login')
+  }
 
   return (
     <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom container pt-3">
@@ -18,7 +30,7 @@ const Navbar = () => {
           <p className="me-3 m-0 py-2 text-dark text-decoration-none">
             {user.username}
           </p>
-          <button className="btn btn-outline-danger">logout</button>
+          <button className="btn btn-outline-danger" onClick={logoutHandler}>logout</button>
           </>
         ) : (
           <>
