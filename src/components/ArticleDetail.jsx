95f86d73197a219023c9744496549a06e62b6a9e
Mirfozil <mirfoziln@gmail.com>
2023-05-28 09:55:39 +0500

    article ui

--- src/components/ArticleDetail.jsx
+++ src/components/ArticleDetail.jsx
@@ -1,28 +1,54 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ArticleService from "../service/article";
-import { useDispatch } from "react-redux";
-import { getArticleDetailFailure, getArticleDetailStart, getArticleDetailSuccess } from "../slice/article";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getArticleDetailFailure,
+  getArticleDetailStart,
+  getArticleDetailSuccess,
+} from "../slice/article";
+import moment from "moment/moment";
+import { Loader } from "../ui";
 
 const ArticleDetail = () => {
   const { slug } = useParams();
-  const dispatch = useDispatch(state => state.article);
+  const dispatch = useDispatch();
+  const { articleDetail, isLoading } = useSelector((state) => state.article);
 
   useEffect(() => {
     const getArticleDetail = async () => {
-        dispatch(getArticleDetailStart())
+      dispatch(getArticleDetailStart());
       try {
         const response = await ArticleService.getArticleDetail(slug);
-        dispatch(getArticleDetailSuccess(response.article))
+        dispatch(getArticleDetailSuccess(response.article));
       } catch (error) {
-        dispatch(getArticleDetailFailure())
+        dispatch(getArticleDetailFailure());
       }
     };
 
     getArticleDetail();
   }, [slug]);
 
-  return <div>{slug}</div>;
+  return isLoading ? (
+    <Loader />
+  ) : (
+    articleDetail !== null && (
+      <div>
+        <div className="p-5 mb-4 rounded-3">
+          <div className="container-fluid py-5">
+            <h1 className="display-5 fw-bold">{articleDetail.title}</h1>
+            <p className="col-md-8 fs-4">{articleDetail.description}</p>
+            <p className="text-muted">
+              <span className="fw-bold">Created at :</span>{" "}
+              {moment(articleDetail.createdAt).format("DD MM, YYYY")}
+            </p>
+            <div className="col-md-6"></div>
+            <div>{articleDetail.body}</div>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default ArticleDetail;
